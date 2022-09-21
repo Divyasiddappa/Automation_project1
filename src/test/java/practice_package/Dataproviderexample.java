666ca5aeaf81b98d2687636132b57e9bf3e64a0f
LAVANYA <LAVANYA@HP>
2022-09-21 12:00:13 +0530

    xml  and program written

---
+++ src/test/java/practice_package/Dataproviderexample.java
@@ -0,0 +1,34 @@
+package practice_package;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Dataproviderexample {
+	@Test(dataProvider ="data")
+public void cityTest(String city, int pincode)
+{
+	System.out.println(Thread.currentThread().getId());
+	System.out.println("city"+city);
+	System.out.println("pincode"+pincode);
+	
+}
+	@DataProvider( name="data")
+public Object[][] getdata()
+{
+	Object[][] obj = new Object[3][2];
+	obj[0][0]="Bangalore";
+	obj[0][1]="560010";
+	obj[1][0]="Mysore";
+	obj[1][1]="570010";
+	obj[2][0]="udupi";
+	obj[2][1]="570017";
+	return obj;
+	
+}
+	
+	
+	
+	
+	
+	
+}
--- src/test/java/testng_practice/FetchdatafromcmdTest.java
+++ src/test/java/testng_practice/FetchdatafromcmdTest.java
@@ -7,9 +7,7 @@ public class FetchdatafromcmdTest {
 	@Test
 	public void step1Test() {
 		String browser = System.getProperty("b");
-		browser="chrome";
 		String url = System.getProperty("u");
-		url="https:gmail.com";
 		System.out.println("browser name is " + browser);
 		System.out.println("specified url is " + url);
 		WebdriverUtility webutility = new WebdriverUtility();
